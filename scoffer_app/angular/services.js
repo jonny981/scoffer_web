06c9aaf5af14d065727c8c164f3f8748fb012c34
jonny981 <jonnyneill@hotmail.com>
2015-03-14 18:19:15 +0000

    Test

--- scoffer_app/angular/services.js
+++ scoffer_app/angular/services.js
@@ -5,7 +5,6 @@
 scofferApp.factory('album', ['$rootScope', '$resource',
   function($rootScope, $resource){
     var url = $.cloudinary.url('myphotoalbum', {format: 'json', type: 'list'});
-    //cache bust
     url = url + "?" + Math.ceil(new Date().getTime()/1000);
     return $resource(url, {}, {
       photos: {method:'GET', isArray:false}
